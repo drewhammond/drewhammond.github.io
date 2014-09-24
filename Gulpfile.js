cd81e0a3b8d8e7c01bdf1b865600228e3c60f82a
Drew Hammond <drew@alphagenetica.com>
2014-09-24 16:36:21 -0400

    CSS3 animation and improved grunt routine

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,50 +1,69 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    plumber = require('gulp-plumber'),
-    watch = require('gulp-watch'),
-    livereload = require('gulp-livereload'),
-    w3cjs = require('gulp-w3cjs');
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	plumber = require('gulp-plumber'),
+	watch = require('gulp-watch'),
+	livereload = require('gulp-livereload'),
+	minifycss = require('gulp-minify-css'),
+	uncss = require('gulp-uncss'),
+	rename = require('gulp-rename');
 
 // Paths to assets
 var paths = {
-    'images': 'assets/img',
-    'fonts': 'assets/fonts',
-    'sass': 'assets/sass',
-    'css': 'assets/css',
-    'js': 'assets/js'
+	'images': 'assets/img',
+	'fonts' : 'assets/fonts',
+	'sass'  : 'assets/sass',
+	'css'   : 'assets/css',
+	'js'    : 'assets/js'
 };
 
 gulp.task('watch', function () {
-    console.log('Starting watch server');
+	console.log('Starting watch server');
 
-    // Sass watch
-    gulp.watch(paths.sass + '/**/*.scss', ['sass']);
+	// Sass watch
+	gulp.watch(paths.sass + '/**/*.scss', ['sass']);
 
-    // HTML watch
-    gulp.watch('index.html', ['html']);
+	// HTML watch
+	gulp.watch('index.html', ['html']);
 
-    // JS watch
-    gulp.watch(paths.js + '/**/*.js', ['js']);
+	// JS watch
+	gulp.watch(paths.js + '/**/*.js', ['js']);
 
 
 });
 
 gulp.task('html', function () {
-    gulp.src('index.html')
-        // Validate HTML5 against W3C validator
-        // .pipe(w3cjs())
-        .pipe(livereload());
+	gulp.src('index.html')
+		// Validate HTML5 against W3C validator
+		// .pipe(w3cjs())
+		.pipe(livereload());
 });
 
 gulp.task('sass', function (e) {
-    return gulp.src(paths.sass + '/**/*.scss')
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer('last 3 versions'))
-        .pipe(gulp.dest(paths.css))
-        .pipe(livereload());
+	return gulp.src(paths.sass + '/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass({
+			precision: 10,
+			imgPath  : 'assets/img'
+		}))
+		.pipe(autoprefixer('last 3 versions'))
+		.pipe(gulp.dest(paths.css))
+		.pipe(minifycss())
+		.pipe(rename({extname: '.min.css'}))
+		.pipe(gulp.dest(paths.css))
+		.pipe(livereload());
 });
+
+gulp.task('optimize', function () {
+	return gulp.src(paths.css + '/main.css')
+		.pipe(uncss({
+			html: ['index.html']
+		}))
+		.pipe(gulp.dest(paths.css))
+		.pipe(minifycss())
+		.pipe(rename({extname: '.min.css'}))
+		.pipe(gulp.dest(paths.css))
+})
 
 //;gulp.task('sass', function () {
 //    return gulp.src(paths.sass + '/**/*')
@@ -56,20 +75,20 @@ gulp.task('sass', function (e) {
 //});
 
 gulp.task('js', function () {
-    console.log(paths.js);
+	console.log(paths.js);
 });
 
 
 // Task for development
 gulp.task('dev', ['watch', 'sass', 'js'], function () {
-    console.log('Dev task completed!');
+	console.log('Dev task completed!');
 });
 
 // Task for production
 gulp.task('prod', ['watch', 'sass', 'js'], function () {
-    console.log('Dev task completed!');
+	console.log('Dev task completed!');
 });
 
 gulp.task('default', ['dev'], function () {
-    console.log('Task completed successfully');
+	console.log('Task completed successfully');
 });
